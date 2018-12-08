169569afda7e46cc30ca3897fa3c596e7f2277d2
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2018-12-08 17:40:38 -0300

    statics

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path('post', views.PostListView.as_view(), name='post-list'),
+    path('post/', views.PostListView.as_view(), name='post-list'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
